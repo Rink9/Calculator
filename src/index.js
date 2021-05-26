7ae2b0aee91f896e440568c0060b7596a8d2ca05
Rinky Chowdhury <rink.chowdhury1559@gmail.com>
2021-05-26 22:58:09 +0600

    Added comments to each functionality

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
+// Initializing variables
 const btns = document.getElementsByClassName("btn");
 const resultText = document.getElementById("result");
 const promptText = document.getElementById("prompt");
 
+// Adding eventListener to each buttons
 [...btns].forEach(btn => {
     btn.addEventListener("click", calculate);
 });
 
+// Calculates the input and renders the result
 function calculate(event) {
+    // Getting clicked element value
     let clickedElementValue = event.target.innerHTML;
+    // Restricting user to enter "0" as the first digit of input
     if (resultText.innerHTML === "" && clickedElementValue === "0") {
         return;
     }
+    // Restricting user to enter greater than 15 character of input
     if (resultText.innerHTML.length >= 15 && clickedElementValue !== "AC" && clickedElementValue !== "=") {
         return;
     }
+    // Handling "=" functionality
     if (clickedElementValue === "=") {
         let result = eval(resultText.innerHTML);
         promptText.innerHTML = resultText.innerHTML;
+        // Displaying decimal point if neccassery by rounding the result
         resultText.innerHTML = Math.round(result * 100) / 100;
-    } else if (clickedElementValue === "AC") {
+    }
+    // Handling "AC" functionality
+    else if (clickedElementValue === "AC") {
+        // Resetting prompt and result text 
         promptText.innerHTML = "";
         resultText.innerHTML = "";
-    } else {
+    }
+    // Handling other inputs
+    else {
         resultText.innerHTML += clickedElementValue;
     }
 
